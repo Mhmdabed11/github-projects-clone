6c9c49f3a2d6bc6750476a82406f15889c1a1ca1
MhmdAbed11 <mohammad_aabed@hotmail.com>
2019-11-10 17:09:11 +0200

    Updates

---
+++ precache-manifest.f52e4fae9b44fd34519bb820a23f0b05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c18a617017f2397b55ab5f2d42aa78a",
+    "url": "/github-projects-clone/index.html"
+  },
+  {
+    "revision": "eb201252149b5cd6f276",
+    "url": "/github-projects-clone/static/css/main.45394a21.chunk.css"
+  },
+  {
+    "revision": "ac6c74e7c0829fffdc7a",
+    "url": "/github-projects-clone/static/js/2.565f8d81.chunk.js"
+  },
+  {
+    "revision": "eb201252149b5cd6f276",
+    "url": "/github-projects-clone/static/js/main.d7c702d1.chunk.js"
+  },
+  {
+    "revision": "5a24381e695ad0ede316",
+    "url": "/github-projects-clone/static/js/runtime~main.c8f7f044.js"
+  }
+]);
